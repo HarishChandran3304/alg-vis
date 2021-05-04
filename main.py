10280cb27b4c5b3f6a0a13a8c23933d48d5596c5
Harish Chandran <harishchandran3304@gmail.com>
2021-05-04 15:42:54 +0530

    Updated window icon and caption

--- main.py
+++ main.py
@@ -15,34 +15,37 @@ SCREEN_HEIGHT = 720
 #SETUP
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+icon = pygame.image.load("Logo.svg")
+pygame.display.set_icon(icon)
+pygame.display.set_caption('Alg-Vis')
 
 
 #HELPER FUNCTIONS
 def main_menu():
-    main_menu = pygame_menu.Menu(720, 1280, 'Welcome', theme=pygame_menu.themes.THEME_DARK)
-    main_menu.add.button('Start', select_menu)
-    main_menu.add.button('Quit', pygame_menu.events.EXIT)
+    main_menu = pygame_menu.Menu(720, 1280, "Welcome", theme=pygame_menu.themes.THEME_DARK)
+    main_menu.add.button("Start", select_menu)
+    main_menu.add.button("Quit", pygame_menu.events.EXIT)
     main_menu.mainloop(screen)
-    
+
 def select_menu():
-    select_menu = pygame_menu.Menu(720, 1280, 'Algorithms', theme=pygame_menu.themes.THEME_DARK)
-    select_menu.add.button('Path-Finding Algorithms', pathfinding_menu)
-    select_menu.add.button('Sorting Algorithms', sorting_menu)
-    select_menu.add.button('Back', main_menu)
+    select_menu = pygame_menu.Menu(720, 1280, "Algorithms", theme=pygame_menu.themes.THEME_DARK)
+    select_menu.add.button("Path-Finding Algorithms", pathfinding_menu)
+    select_menu.add.button("Sorting Algorithms", sorting_menu)
+    select_menu.add.button("Back", main_menu)
     select_menu.mainloop(screen)
 
 def pathfinding_menu():
-    main_menu = pygame_menu.Menu(720, 1280, 'Path-Finding Algorithms', theme=pygame_menu.themes.THEME_DARK)
-    main_menu.add.button('A* Search', foo)
-    main_menu.add.button('Dijkstra\'s Algorithm', foo)
-    main_menu.add.button('Back', select_menu)
+    main_menu = pygame_menu.Menu(720, 1280, "Path-Finding Algorithms", theme=pygame_menu.themes.THEME_DARK)
+    main_menu.add.button("A* Search", foo)
+    main_menu.add.button("Dijkstra\'s Algorithm", foo)
+    main_menu.add.button("Back", select_menu)
     main_menu.mainloop(screen)
     
 def sorting_menu():
-    main_menu = pygame_menu.Menu(720, 1280, 'Sorting Algorithms', theme=pygame_menu.themes.THEME_DARK)
-    main_menu.add.button('Bubble Sort', foo)
-    main_menu.add.button('Merge Sort', foo)
-    main_menu.add.button('Back', select_menu)
+    main_menu = pygame_menu.Menu(720, 1280, "Sorting Algorithms", theme=pygame_menu.themes.THEME_DARK)
+    main_menu.add.button("Bubble Sort", foo)
+    main_menu.add.button("Merge Sort", foo)
+    main_menu.add.button("Back", select_menu)
     main_menu.mainloop(screen)
 
 def foo():
@@ -50,7 +53,7 @@ def foo():
 
 
 #MAIN LOOP
-def mainloop():
+def main():
     running = True
     while running:
         for event in pygame.event.get():
@@ -61,4 +64,4 @@ def mainloop():
 
 #MAIN CALL
 if __name__ == "__main__":
-    mainloop()
+    main()
