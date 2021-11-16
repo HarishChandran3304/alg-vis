cf45db758b632783855aa9da17b81a5e93a76214
Devansh Sharma <sdevansh772@gmail.com>
2021-11-17 00:51:54 +0530

    Shows subject topper as well now

---
+++ Algorithms/Sorting/UserInput/skfblehjf.py
@@ -0,0 +1,7 @@
+a = {90: 'Devansh', 69: 'Harish', 45: 'Random', 50: 'abcd', 97: 'HIHIHIHI', 56: 'MY NAME IS'}
+names, marks = list(a.values()), list(a.keys())
+test = 50
+print(marks)
+marks.sort(reverse=True)
+print(marks)
+# index = marks.index[]
