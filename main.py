2b5d73ffd02ec44c447dad13feb549701245d3d4
Harish Chandran <harishchandran3304@gmail.com>
2022-01-12 09:46:14 +0530

    Finished Pathfinding

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #IMPORTS
 import pygame
 import pygame_menu
-from Algorithms.PathFinding.AStarSearch import astar
-from Algorithms.PathFinding.DijkstrasAlgorithm import dijkstra
+from Algorithms.PathFinding import pathfinding
 from Algorithms.Sorting.BubbleSort import bubblesort
 from Algorithms.Sorting.MergeSort import mergesort
 from Algorithms.Sorting.QuickSort import quicksort
@@ -34,21 +33,12 @@ def main_menu():
 
 def select_menu():
     selectmenu = pygame_menu.Menu(720, 1280, "Algorithms", theme=pygame_menu.themes.THEME_DARK)
-    selectmenu.add.button("Path-Finding Algorithms", pathfinding_menu, font_size=50)
+    selectmenu.add.button("Path-Finding Algorithms", launch_pathfinding_client, font_size=50)
     selectmenu.add.vertical_margin(50)
     selectmenu.add.button("Sorting Algorithms", sorting_menu, font_size=50)
     selectmenu.add.vertical_margin(150)
     selectmenu.add.button("Back", main_menu, font_size=50)
     selectmenu.mainloop(screen)
-
-def pathfinding_menu():
-    pfmenu = pygame_menu.Menu(720, 1280, "Path-Finding Algorithms", theme=pygame_menu.themes.THEME_DARK)
-    pfmenu.add.button("A* Search", launch_astar, font_size=50)
-    pfmenu.add.vertical_margin(50)
-    pfmenu.add.button("Dijkstra\'s Algorithm", launch_dijkstra, font_size=50)
-    pfmenu.add.vertical_margin(150)
-    pfmenu.add.button("Back", select_menu, font_size=50)
-    pfmenu.mainloop(screen)
     
 def sorting_menu():
     smenu = pygame_menu.Menu(720, 1280, "Sorting Algorithms", theme=pygame_menu.themes.THEME_DARK)
@@ -61,11 +51,8 @@ def sorting_menu():
     smenu.add.button("Back", select_menu, font_size=50)
     smenu.mainloop(screen)
 
-def launch_astar():
-    astar.main(screen, 720)
-
-def launch_dijkstra():
-    dijkstra.main(screen, 720)
+def launch_pathfinding_client():
+    pathfinding.main(screen, 720)
 
 def launch_bubblesort():
     bubblesort.main()
@@ -75,9 +62,6 @@ def launch_mergesort():
 
 def launch_quicksort():
     quicksort.main()
-
-def foo():
-    pass
 
 
 #MAIN LOOP
